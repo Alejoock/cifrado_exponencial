16c5fd8d7e8320de92fe6450c96fa74b364b47f4
Alejandro <sagomeza@correo.udistrital.edu.co>
2023-05-28 10:36:34 -0500

    Se introduce el archivo bot.py para la conexión con Telegram

---
+++ bot.py
@@ -0,0 +1,44 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+# Lo basico
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")    
+###
+
+
+
+
+
+if __name__ == '__main__':
+
+# Lo basico    
+    application = ApplicationBuilder().token('6270632611:AAFi1Ux1THMpYP1Sd27w_S1p2Mj7ByELeNo').build()
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    start_handler = CommandHandler('startt', start)
+    caps_handler = CommandHandler('caps', caps)
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(caps_handler)
+    application.add_handler(unknown_handler)
+###    
+    application.run_polling()
